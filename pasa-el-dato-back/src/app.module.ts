fd430b51038d38d7422be2ac1e67c91fe56782a4
Joaquin <jo.perezu@duocuc.cln>
2026-09-08 21:02:41 -0300

    fix(docker.compose) docker ajustado para backend

--- pasa-el-dato-back/src/app.module.ts
+++ pasa-el-dato-back/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
+import { HealthController } from './health.controller.js';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, HealthController],
   providers: [AppService],
 })
 export class AppModule {}
