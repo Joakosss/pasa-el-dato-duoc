525c4628c33e1852eb33a841ed592d1e44050484
Zireaeell <caro.pacheco@duocuc.cl>
2026-09-15 00:16:31 -0300

    swagger

--- pasa-el-dato-back/src/main.ts
+++ pasa-el-dato-back/src/main.ts
@@ -1,9 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  
   const app = await NestFactory.create(AppModule);
   //Cuando una ruta recibe un DTO, el ValidationPipe ejecuta las validaciones
   app.useGlobalPipes(
@@ -18,5 +18,16 @@ async function bootstrap() {
   const port = Number(process.env.API_PORT ?? process.env.PORT ?? 3001);
   // 0.0.0.0 es clave para que Podman exponga el back a la LAN.
   await app.listen(port, '0.0.0.0');
+
+  const configuracionSwagger = new DocumentBuilder()
+    .setTitle('Pasa el Dato Duoc API')
+    .setDescription('API para la aplicación Pasa el Dato')
+    .setVersion('1.0.0')
+    .build();
+
+  const documentoSwagger = () =>
+    SwaggerModule.createDocument(app, configuracionSwagger);
+  
+  SwaggerModule.setup('/docs', app, documentoSwagger());
 }
 await bootstrap();
